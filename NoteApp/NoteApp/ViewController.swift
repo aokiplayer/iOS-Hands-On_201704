2e868f54739cee3164bc9486092e5d339b68fbde
Hiroyuki Aoki <aokiplayer@icloud.com>
2017-04-12 13:57:08 +0900

    Connect views
    - UITextField - Outlet
    - UITextView - Outlet
    - UIButton - Action

--- NoteApp/NoteApp/ViewController.swift
+++ NoteApp/NoteApp/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+
+
+    /// 追加ボタンがタップされた際に実行されるメソッド
+    @IBAction func addNote(_ sender: UIButton) {
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
