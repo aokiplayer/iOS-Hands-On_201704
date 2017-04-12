2d133cd077071e3794bcabd5f15709d239cdcf5d
Hiroyuki Aoki <aokiplayer@icloud.com>
2017-04-12 14:09:50 +0900

    Input and Output
    Getting input text from UITextField and putting text to UITextView.

--- NoteApp/NoteApp/ViewController.swift
+++ NoteApp/NoteApp/ViewController.swift
@@ -16,18 +16,26 @@ class ViewController: UIViewController {
 
     /// 追加ボタンがタップされた際に実行されるメソッド
     @IBAction func addNote(_ sender: UIButton) {
+        // メモに追加するメソッドを実行
+        self.addNewNote()
     }
 
+    /// 入力内容をメモに追加するメソッド
+    private func addNewNote() {
+        // テキストフィールドの入力値を取得
+        let line = textField.text
+
+        // 入力内容を、テキストビューに表示
+        textView.text = line
+    }
+
+    /// 画面が読み込まれ、ビューが準備完了後に呼ばれるメソッド
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    /// メモリ不足の際に呼ばれるメソッド
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
