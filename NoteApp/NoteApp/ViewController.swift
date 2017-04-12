ea77c55f9ecccfb06c26f0d1b8e3495a73055757
Hiroyuki Aoki <aokiplayer@icloud.com>
2017-04-12 16:04:05 +0900

    Multi line
    Append input text to text on UITextView.

--- NoteApp/NoteApp/ViewController.swift
+++ NoteApp/NoteApp/ViewController.swift
@@ -22,16 +22,33 @@ class ViewController: UIViewController {
 
     /// 入力内容をメモに追加するメソッド
     private func addNewNote() {
-        // テキストフィールドの入力値を取得
-        let line = textField.text
+        // このメソッドを抜ける直前に、テキストフィールドの入力内容をクリア
+        defer {
+            textField.text = ""
+        }
 
-        // 入力内容を、テキストビューに表示
-        textView.text = line
+        // テキストフィールドの入力値を取得（未入力なら何もせずにメソッドを抜ける）
+        guard let line = textField.text, !line.isEmpty else { return }
+
+        let newNote: String
+
+        // テキストビューに表示されている文字列を取得できた場合は、改行文字と入力値を追加
+        if let currentNote = textView.text, !currentNote.isEmpty {
+            newNote = "\(line)\n\(currentNote)"
+        } else {
+            newNote = line
+        }
+
+        // 文字列を、テキストビューに表示
+        textView.text = newNote
     }
 
     /// 画面が読み込まれ、ビューが準備完了後に呼ばれるメソッド
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // テキストビューの表示内容を、あらかじめクリアしておく
+        textView.text = ""
     }
 
     /// メモリ不足の際に呼ばれるメソッド
